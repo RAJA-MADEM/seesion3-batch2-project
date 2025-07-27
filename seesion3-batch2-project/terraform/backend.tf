c315e12fb3024fdd8421973a31a0e4a496a89e9f
RAJA-MADEM <95684238+RAJA-MADEM@users.noreply.github.com>
2025-07-27 20:34:24 +0530

    Update backend.tf

--- seesion3-batch2-project/terraform/backend.tf
+++ seesion3-batch2-project/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "kundan-terraform-state-bucket"
+    bucket  = "raja-terraform-state-bucket"
     key     = "global/s3/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
